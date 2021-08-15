7caa55b6e5070e5128d4ffc973aec63567fb6daf
madilyani <madilyani1@gmail.com>
2021-08-15 19:14:25 +0500

    12345

---
+++ assets/js/agentRegister.js
@@ -0,0 +1,10 @@
+$(".testimonialAgent__slider").slick({
+  dots: true,
+  infinite: true,
+  arrows: true,
+  prevArrow: $(".testPrev"),
+  nextArrow: $(".testNext"),
+  speed: 500,
+  fade: true,
+  cssEase: "linear",
+});
